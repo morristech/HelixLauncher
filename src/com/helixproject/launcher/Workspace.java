6a6c6c86baa08cfefe7265bc4c14aa4b66976067
Mohd Faruq <faruq.91@gmail.com>
2010-03-10 20:57:41 +0800

    Added velocity scrolling / rosie-like

--- src/com/helixproject/launcher/Workspace.java
+++ src/com/helixproject/launcher/Workspace.java
@@ -50,7 +50,8 @@ public class Workspace extends ViewGroup implements DropTarget, DragSource, Drag
     /**
      * The velocity at which a fling gesture will cause us to snap to the next screen
      */
-    private static final int SNAP_VELOCITY = 1000;
+	// Faruq: Lesser force for fling to occur
+    private static final int SNAP_VELOCITY = 200;
 
     private int mDefaultScreen;
 
@@ -733,10 +734,10 @@ public class Workspace extends ViewGroup implements DropTarget, DragSource, Drag
 
                 if (velocityX > SNAP_VELOCITY && mCurrentScreen > 0) {
                     // Fling hard enough to move left
-                    snapToScreen(mCurrentScreen - 1);
+                    snapToScreen(mCurrentScreen - 1, velocityX);
                 } else if (velocityX < -SNAP_VELOCITY && mCurrentScreen < getChildCount() - 1) {
                     // Fling hard enough to move right
-                    snapToScreen(mCurrentScreen + 1);
+                    snapToScreen(mCurrentScreen + 1, velocityX);
                 } else {
                     snapToDestination();
                 }
@@ -763,7 +764,14 @@ public class Workspace extends ViewGroup implements DropTarget, DragSource, Drag
     }
 
     void snapToScreen(int whichScreen) {
+        snapToScreen(whichScreen, 0);
+    }
+
+	void snapToScreen(int whichScreen, int velocityX) {
         if (!mScroller.isFinished()) return;
+
+		// Faruq: Make revert easing slower
+		int durationOffset = 0;
 
         clearVacantCache();
         enableChildrenCache();
@@ -777,11 +785,18 @@ public class Workspace extends ViewGroup implements DropTarget, DragSource, Drag
         if (focusedChild != null && changingScreens && focusedChild == getChildAt(mCurrentScreen)) {
             focusedChild.clearFocus();
         }
+
+		if (!changingScreens) durationOffset = 600;
         
         final int newX = whichScreen * getWidth();
         final int delta = newX - mScrollX;
-        mScroller.startScroll(mScrollX, 0, delta, 0, Math.abs(delta) * 2);
-        invalidate();
+
+		// Faruq: Velocity Scrolling
+		if (velocityX == 0) 
+        	mScroller.startScroll(mScrollX, 0, delta, 0, Math.abs(delta) * 2 + durationOffset);
+    	else
+			mScroller.startScroll(mScrollX, 0, delta, 0, ((Math.abs(delta) / (Math.abs(velocityX) / 100))*20) + durationOffset);
+		invalidate();
     }
 
     void startDrag(CellLayout.CellInfo cellInfo) {
