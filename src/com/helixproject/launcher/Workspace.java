21ffaf1d91c3a2fa0162c096ed5ad2c4a153709d
Mohd Faruq <faruq.91@gmail.com>
2010-03-11 23:20:11 +0800

    Fixed double tap bug: Reduced sensitivity

--- src/com/helixproject/launcher/Workspace.java
+++ src/com/helixproject/launcher/Workspace.java
@@ -112,7 +112,7 @@ public class Workspace extends ViewGroup implements DropTarget, DragSource, Drag
 	// Faruq: new properties
 	private int mScreenLoaded = 0;
 	private long lastTapTime = 0;
-	private static final long DOUBLE_TAP_TIME = 350;
+	private static final long DOUBLE_TAP_TIME = 250;
 	
     /**
      * Used to inflate the Workspace from XML.
@@ -640,6 +640,9 @@ public class Workspace extends ViewGroup implements DropTarget, DragSource, Drag
                         final View currentScreen = getChildAt(mCurrentScreen);
                         currentScreen.cancelLongPress();
                     }
+
+					// Faruq: Cancel any double taps
+					lastTapTime = 0;
                 }
                 break;
 
@@ -655,7 +658,7 @@ public class Workspace extends ViewGroup implements DropTarget, DragSource, Drag
 					//Log.d("Launcher", "Double tapped");
 					lastTapTime = 0;
 					
-					if (mLauncher != null) {
+					if (mLauncher != null && mScroller.isFinished()) {
 						mLauncher.onDoubleTap();
 						return true;
 					}
